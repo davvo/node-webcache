c3224919eeee922dfcf85fbf5ae5978dd45875ae
David <david.ershag@gmail.com>
2014-02-15 00:29:42 +0100

    Added lib/layer.js

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
 var express = require('express'),
-    MetaTile = require('./lib/metatile.js'),
+    Layer = require('./lib/layer'),
+    MetaTile = require('./lib/metatile'),
     config = require('./config.json');
 
+var layers = {};
+
+Object.keys(config.layers).forEach(function (name) {
+    layers[name] = new Layer(name, config.layers[name]);
+});
+
 var app = express();
-
-function buildUrl(layer, metaTile) {
-    var params = [];
-    params.push('service=WMS');
-    params.push('version=1.1.0');
-    params.push('request=GetMap');
-    params.push('format=' + layer.format);
-    params.push('srs=' + layer.srs);
-    params.push('layers=' + layer.name);
-    params.push('width=' + metaTile.width);
-    params.push('height=' + metaTile.height);
-
-    var bounds = metaTile.tileLatLonBounds();
-    params.push('bbox=' + [bounds.min.lon, bounds.min.lat, bounds.max.lon, bounds.max.lat].join(','));
-
-    return layer.url + '?' + params.join('&');
-};
 
 app.get('/:layer/:z/:x/:y.:type', function (req, res) {
     var x = parseInt(req.params.x, 10),
         y = parseInt(req.params.y, 10),
         z = parseInt(req.params.z, 10);
 
-    var layer = config.layers[req.params.layer];
-    layer.name = req.params.layer;
+    var layer = layers[req.params.layer];
 
-    var metaTile = new MetaTile(x, y, z, layer.metaTile);
-    var url = buildUrl(layer, metaTile);
+    var metaTile = layer.metaTile(x, y, z);
+    var url = layer.buildUrl(metaTile);
     console.log(url);
 
     res.send(metaTile.tileLatLonBounds());
